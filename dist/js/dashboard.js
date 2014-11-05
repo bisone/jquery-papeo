622dfdbbdec7809c828062487d6f9925027a69da
Will@Desktop <changwei.cn@gmail.com>
2014-11-05 09:07:43 +0800

    可以读取json文件显示菜单

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -1,52 +1,66 @@
 (function(){ 
-$.widget("bisone.soneLeftMenu", {
+$.widget("ui.soneLeftMenu", {
     options: {
         value: 0
     },
 
     _create: function() {
         this.element.addClass("wrap");
-        this._update();
+        var scope = this;
+
+        $.ajax({
+            url: "/json/left.menu.json",
+            type: "GET",
+            success: function(mydata) {
+
+                var tpl = ' <div class="sone-left-menu">';
+                $.each(mydata, function(n, item){
+                    tpl += '<div id="market" class="item item-1">'+
+                        '<div class="itm-lv1">'+
+                        '<div class="tit">'+
+                        '<span class="'+item.iconClass+'"></span>'+
+                        '<span>'+item.name+'</span>'+
+                        '<s>3</s>'+
+                        '</div>'+
+                        '<div class="con">'+
+                        '<div class="info">';
+                    $.each(item.info, function (n,i) {
+                        tpl +='<span><b>'+i.value+'</b>'+i.lable+'</span>';
+                    });
+                    tpl +='</div>'+
+                        '<div class="price">'+
+                        '<strong>'+item.price.value+'</strong>'+item.price.lable+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'+
+                        '<div class="itm-lv2">'+
+                        '<ul>';
+                    $.each(item.children, function (n, ch) {
+                        tpl += '<li><a href="'+ch.url+'">'+ch.name+'</a></li>';
+                    });
+                    tpl +='</ul>'+
+                        '</div>'+
+                        '</div>'; });
+
+                tpl += '<div class="resizer"><div class="icon icon-ic_arrow_forward_black_24dp "></div></div>'+
+                    '</div>';
+
+                scope.element.html(tpl);
+
+                scope._update(mydata);
+            }});
     },
 
     _setOption: function(key, value) {
         this.options[key] = value;
-        this._update();
+
     },
 
-    _update: function() {
+    _update: function(data) {
+        console.log(data);
 
-        var leftMenuTemplate=' <div class="sone-left-menu">'+
-        '<div id="market" class="item item-1">'+
-          '<div class="itm-lv1">'+
-            '<div class="tit">'+
-              '<span class="icon icon-ic_play_circle_outline_black_24dp"></span>'+
-              '<span>市场</span>'+
-              '<s>3</s>'+
-            '</div>'+
-            '<div class="con">'+
-              '<div class="info">'+
-                '<span><b>0</b>部门</span>'+
-                '<span><b>0</b>品类</span>'+
-              '</div>'+
-              '<div class="price">'+
-                '<strong>1200</strong>万元'+
-              '</div>'+
-            '</div>'+
-          '</div>'+
-          '<div class="itm-lv2">'+
-            '<ul>'+
-              '<li><a href="#">市场行情</a></li>'+
-              '<li><a href="#">市场集中度</a></li>'+
-              '<li><a href="#">市场竞争强度</a></li>'+
-              '<li><a href="#">市场需求度</a></li>'+
-            '</ul>'+
-          '</div>'+
-        '</div>'+
-        '<div class="resizer"><div class="icon icon-ic_arrow_forward_black_24dp "></div></div>'+
-      '</div>';
 
-        this.element.html(_.template(leftMenuTemplate, {data: this.options}));
+
         this._initEvents();
     },
 
@@ -71,7 +85,7 @@ $.widget("bisone.soneLeftMenu", {
         });
 
         $(".sone-left-menu .item", _ele).hover(function(){
-            if($(".wrap", _ele).hasClass("narrow-wrap")){
+            if($(".wrap").hasClass("narrow-wrap")){
                 if($(this).hasClass("item-open")){
                     isOpen=true;
                 }
@@ -81,7 +95,7 @@ $.widget("bisone.soneLeftMenu", {
                 $(this).addClass("item-open").find(".itm-lv2").show();
             }
         },function(){
-            if($(".wrap", _ele).hasClass("narrow-wrap")){
+            if($(".wrap").hasClass("narrow-wrap")){
                 if(!isOpen){
                     $(this).removeClass("item-open");
                 }
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp    = require('gulp'),
 
 var paths = {
     js: 'src/js/**/*.*',
+    json: 'src/json/**/*.*',
     fonts: 'src/fonts/**.*',
     images: 'src/img/**/*.*',
     styles: 'src/less/**/*.less',
@@ -28,11 +29,16 @@ gulp.task('usemin', function() {
 /**
  * Copy assets
  */
-gulp.task('copy-assets', ['copy-images', 'copy-fonts',  'copy-bower_fonts']);
+gulp.task('copy-assets', ['copy-images', 'copy-fonts',  'copy-bower_fonts','copy-json']);
 
 gulp.task('copy-images', function(){
     return gulp.src(paths.images)
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('copy-json', function(){
+    return gulp.src(paths.json)
+        .pipe(gulp.dest('dist/json'));
 });
 
 
@@ -54,6 +60,7 @@ gulp.task('copy-bower_fonts', function(){
 gulp.task('watch', function () {
     gulp.watch([paths.styles, paths.index, paths.js], ['usemin']);
     gulp.watch([paths.images], ['copy-images']);
+    gulp.watch([paths.json], ['copy-json']);
     gulp.watch([paths.fonts], ['copy-fonts']);
     gulp.watch([paths.bower_fonts], ['copy-bower_fonts']);
 });
